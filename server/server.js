967a941a45188ac458f9e956f7c2a042fc14e38a
Ash Tan <ashtan19@gmail.com>
2018-06-30 18:10:58 -0700

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -10,9 +10,10 @@ heroku config ~This will show the mongodb uri that your app can connect to
 git push heroku master
 */
 
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 const {ObjectID} = require("mongodb");
+const _ = require("lodash");
 
 var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
@@ -86,6 +87,35 @@ app.delete("/todos/:id", (req, res) => {
         console.log("ERROR: Unable to get ID and delete todo");
     })
 });
+
+app.patch("/todos/:id", (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']); //Subset of the properties that the user passed to the program
+
+    if (!ObjectID.isValid(id)) {
+        console.log("ERROR: Todo ID not valid");
+        return res.status(404).send();
+    };
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            console.log("ERROR: No todo to find and update");
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }).catch((e) => {
+        console.log("ERROR");
+        res.status(400).send();
+    })
+});
+
 
 
 
